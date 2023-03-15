6829b7b8a08711827f5bfff0b6d20fc3f7d0f4aa
jhn <xxxjiangxxx@qq.com>
2023-03-15 20:53:19 +0800

    加入了jwt鉴权以及token生成与验证函数

--- middleware/AuthMiddleWare.go
+++ middleware/AuthMiddleWare.go
@@ -6,18 +6,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 func AuthMiddleWare() gin.HandlerFunc  {
-	return func(ctx *gin.Context) {
-		tokenstring := ctx.Query("token")
+	return func(c *gin.Context) {
+		tokenstring := c.Query("token")
 		token,claims,err := ParseToken(tokenstring);
 		if err!=nil||!token.Valid{
-			ctx.JSON(http.StatusUnauthorized,gin.H{
-				"msg":"权限不足",
+			c.JSON(http.StatusUnauthorized,gin.H{
+				"status_code":1,
+				"status_msg":"权限不足",
 			})
-			ctx.Abort()
+			c.Abort()
 			return
 		}
-		ctx.Set("uid",claims.userid)
-		ctx.Next()
+		c.Set("uid",claims.userid)
+		c.Next()
 		
 	}
 }
